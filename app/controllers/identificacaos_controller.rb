0266ab72ec8d6e7783c8aaf212b22b0279806d1b
Manutencao <alexandrejr_soares@yahoo.com.br>
2012-10-23 14:46:19 -0200

    all

--- app/controllers/identificacaos_controller.rb
+++ app/controllers/identificacaos_controller.rb
@@ -43,25 +43,26 @@ class IdentificacaosController < ApplicationController
     redirect_to identificacaos_url
   end
 
-def consultaTit
+def consulta_tit
+  
   unless params[:search].present?
     if params[:type_of].to_i == 3
-      @contador = Identificacao.all.count
-      @identificacaos = Identificacao.paginate :all,:page => params[:page], :order => 'livro ASC', :per_page => 20
+      @contador = Identificacao.all(:include => [:livros => [:localizacao]], :conditions => ["localizacoes.unidade_id = ?", current_user.unidade_id]).count
+      @identificacaos = Identificacao.paginate :all, :include => [:livros => [:localizacao]], :conditions => ["localizacoes.unidade_id = ?", current_user.unidade_id],:page => params[:page], :order => 'livro ASC', :per_page => 20
       render :update do |page|
         page.replace_html 'titulos', :partial => "titulos"
       end
     end
   else
     if params[:type_of].to_i == 1
-      @contador = Identificacao.all(:conditions => ["livro like ?", "%" + params[:search].to_s + "%"]).count
-      @identificacaos = Identificacao.paginate :all, :page => params[:page], :per_page => 20, :conditions => ["livro like ?", "%" + params[:search].to_s + "%"],:order => 'livro ASC'
+      @contador = Identificacao.all(:include => [:livros => [:localizacao]],:conditions => ["livro like ? and localizacoes.unidade_id = ?", "%" + params[:search].to_s + "%",current_user.unidade_id]).count
+      @identificacaos = Identificacao.paginate :all,:include => [:livros => [:localizacao]], :page => params[:page], :per_page => 20, :conditions => ["livro like ?  and localizacoes.unidade_id = ?", "%" + params[:search].to_s + "%",current_user.unidade_id],:order => 'livro ASC'
       render :update do |page|
         page.replace_html 'titulos', :partial => "titulos"
       end
       else if params[:type_of].to_i == 2
-        @contador = Identificacao.all(:conditions => ["subtitulo like ?", "%" + params[:search].to_s + "%"]).count
-        @identificacaos = Identificacao.paginate :all, :page => params[:page], :per_page => 20, :conditions => ["subtitulo like ?", "%" + params[:search].to_s + "%"],:order => 'livro ASC'
+        @contador = Identificacao.all(:include => [:livros => [:localizacao]],:conditions => ["subtitulo like ? and localizacoes.unidade_id = ?", "%" + params[:search].to_s + "%", current_user.unidade_id]).count
+        @identificacaos = Identificacao.paginate :all,:include => [:livros => [:localizacao]], :page => params[:page], :per_page => 20, :conditions => ["subtitulo like ? and localizacoes.unidade_id = ?", "%" + params[:search].to_s + "%", current_user.unidade_id],:order => 'livro ASC'
         render :update do |page|
           page.replace_html 'titulos', :partial => "titulos"
         end
