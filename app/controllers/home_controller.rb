7afe9349d8bf7da7178995130123966e0331425d
Manutencao <alexandrejr_soares@yahoo.com.br>
2013-02-07 10:46:42 -0200

    all

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,10 +11,15 @@ class HomeController < ApplicationController
   end
 
   def load_resources
-    if current_user.unidade_id == 53
-      @classes = Aluno.all(:select => "id_classe, classe_descricao, classe_ano, id_escola",:conditions => ["classe_ano = ?", Date.today.strftime("%Y").to_i], :group => ["id_classe,classe_descricao, classe_ano,id_escola"] , :order => "classe_descricao")
-    else
-      @classes = Aluno.all(:select => "id_classe, classe_descricao, classe_ano, id_escola",:conditions => ["classe_ano = ? and id_escola = ?", Date.today.strftime("%Y").to_i, current_user.unidade.unidades_gpd_id], :group => ["id_classe,classe_descricao, classe_ano,id_escola"] , :order => "classe_descricao")
+    begin
+      session[:falha] = nil
+      if current_user.unidade_id == 53
+        @classes = Aluno.all(:select => "id_classe, classe_descricao, classe_ano, id_escola",:conditions => ["classe_ano = ?", Date.today.strftime("%Y").to_i], :group => ["id_classe,classe_descricao, classe_ano,id_escola"] , :order => "classe_descricao")
+      else
+        @classes = Aluno.all(:select => "id_classe, classe_descricao, classe_ano, id_escola",:conditions => ["classe_ano = ? and id_escola = ?", Date.today.strftime("%Y").to_i, current_user.unidade.unidades_gpd_id], :group => ["id_classe,classe_descricao, classe_ano,id_escola"] , :order => "classe_descricao")
+      end
+    rescue
+      session[:falha] = "Impossivel conectar ao servidor. Emprestimos e Devoluções desabilitadas"
     end
      #@disponiveis = Dpu.all(:include => [:livro =>[:identificacao]],:conditions => ["(dpus.livro_id is not null) and dpus.status = 1 and dpus.unidade_id = ?", current_user.unidade_id],:order => "identificacaos.livro ASC")
   end
