d071bff5be4191dfb8ffa038e8e7afd38969e3bd
Analistas <naor@seducpma.com>
2013-06-21 13:31:16 -0300

    acerto sem gpd

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "192.168.0.122"
-set :repository, "git://github.com/alexsoares/biblioteca.git"
+set :repository, "git://github.com/seducpma/biblioteca.git"
 set :user, "servidor"
 set :use_sudo, false
 set :deploy_to, "/home/#{user}/novo.biblioteca.seducpma.com"
