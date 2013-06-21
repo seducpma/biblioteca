4754dc69103411cb9d840b10a701dab94c31b0a1
Analistas <analistas@analistas-desktop.(none)>
2013-06-21 13:24:52 -0300

    sem gpd

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-# This controller handles the login/logout function of the site.  
+# This controller handles the login/logout function of the site.
 class SessionsController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    logout_keeping_session!
+   logout_keeping_session!
     user = User.authenticate(params[:login], params[:password])
     if user
       # Protects against session fixation attacks, causes request forgery
@@ -18,15 +18,8 @@ class SessionsController < ApplicationController
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
-      ambiente = Ambiente.find_all_by_user_id(user)
-      #@ambiente = ambiente.last
-      if ambiente.present?
-        redirect_back_or_default(home_path(:user => user, :type => 0))
-      else
-        redirect_to(ambiente_configuracoes_path)
-      end
-      flash[:notice] = "BEM VINDO AO BIBLOS"
-
+      redirect_back_or_default('/')
+      flash[:notice] = "BEM VINDO AO SISCAP"
     else
       note_failed_signin
       @login       = params[:login]
@@ -35,16 +28,17 @@ class SessionsController < ApplicationController
     end
   end
 
+
   def destroy
     logout_killing_session!
-    flash[:notice] = "ATÉ MAIS....."
+    flash[:notice] = "VOCẼ ACABOU DE SAIR DO SISCAP"
     redirect_back_or_default('/')
   end
 
 protected
   # Track failed login attempts
   def note_failed_signin
-    flash[:error] = "ACESSO NÃO AUTORIZADO '#{params[:login]}'"
-    logger.warn "ACESSO NÃO AUTORIZADO '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
+    flash[:error] = "USUÁRIO NÂO AUTORIZADO '#{params[:login]}'"
+    logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
 end
