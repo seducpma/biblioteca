deb9c985d30692ee681973f4a36b9414a07858b1
seduc <seduc@seduc1.(none)>
2015-04-30 09:28:22 -0300

    biblioteca

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
       redirect_back_or_default('/')
-      flash[:notice] = "BEM VINDO AO SISCAP"
+      flash[:notice] = "BEM VINDO AO BIBLOS"
     else
       note_failed_signin
       @login       = params[:login]
@@ -31,7 +31,7 @@ class SessionsController < ApplicationController
 
   def destroy
     logout_killing_session!
-    flash[:notice] = "VOCẼ ACABOU DE SAIR DO SISCAP"
+    flash[:notice] = "VOCẼ ACABOU DE SAIR DO BIBLOS"
     redirect_back_or_default('/')
   end
 
